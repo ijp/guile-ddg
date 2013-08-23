509c1801774d86e248ed9eefd97c3fad205051c9
Ian Price <ianprice90@googlemail.com>
2013-08-23 02:40:16 +0100

    Add license info

--- ddg.scm
+++ ddg.scm
@@ -1,3 +1,16 @@
+;;; ddg.scm --- Duck Duck Go Api Access
+
+;; Copyright (C) 2013 Ian Price <ianprice90@googlemail.com>
+
+;; Author: Ian Price <ianprice90@googlemail.com>
+
+;; This program is free software, you can redistribute it and/or
+;; modify it under the terms of the new-style BSD license.
+
+;; You should have received a copy of the BSD license along with this
+;; program. If not, see <http://www.debian.org/misc/bsd.license>.
+
+;;; Code:
 (define-module (ddg)
   #:use-module (web uri)
   #:use-module (web client)
